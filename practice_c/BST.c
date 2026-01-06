7e866341fc70e618711f42faffeb556b63bfdf17
Intisar-Ahmad <intisar12122000@gmail.com>
2026-01-06 21:54:17 +0500

    BST in C

--- practice_c/BST.c
+++ practice_c/BST.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include<stdlib.h>
 
 // Node struct
 typedef struct Node{
@@ -8,9 +9,49 @@ typedef struct Node{
 }Node;
 
 
+Node* createNode(int data){
+    Node* node = (Node*)malloc(sizeof(Node));
+    node->data = data;
+    node->right = node->left = NULL;
+    return node;
+
+}
+
+// void insertNode
+Node* insertNode(Node* root,int data){
+    if(!root){
+        return createNode(data);
+    }
+    if(data < root->data){
+        root->left = insertNode(root->left,data);
+    }
+    else{ 
+        root->right = insertNode(root->right,data);
+    }
+    return root;
+}
+
+// preorder traversal
+void preOrder(Node* root){
+    if(!root) return;
+
+    printf("%d ",root->data);
+    preOrder(root->left);
+    preOrder(root->right);
+
+}
 
 
 int main() {
- 
+    Node* root = createNode(5);
+    root = insertNode(root,2);
+    root = insertNode(root,3);
+    root = insertNode(root,1);
+    root = insertNode(root,6);
+    root = insertNode(root,8);
+
+
+    preOrder(root);
+    printf("\n");
     return 0;
 }
